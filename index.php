cf20b604b64109da75235deb0d7632478ccf07ec
hesarnurramaduloh <hesarnur@gmail.com>
2017-02-06 15:24:29 +0700

    Tugas 2

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<html>
+<title>Halaman Awal Operasi CRUD</TITLE>
+<style>
+div.container {
+	width: 100%;
+	border 1px solid gray;
+}
+header, footer {
+	padding: 0.5cm;
+	color: white;
+	background-color: #0000CC;
+	clear: left;
+	text-align: center;
+	}
+	
+nav {
+	float: left;
+	max-width: 160px;
+	margin: 0;
+	padding: 1cm;
+}
+nav ul {
+	list-style-type: none;
+	padding: 0;
+}
+nav ul a {
+	text-decoration: none;
+}
+artikle{
+margin-left: 170px;
+berder-left: 1px colid gray;
+padding :1px;   
+overflow; hidden;
+}
+</style>
+</head>
+<body>
+
+<div class-"container">
+<header> 
+<h1>Operasi CRUD</h1>
+<h3>(Create Read Update Delete)</h3>
+</header>
+
+<nav>
+	<ul>
+	<li><b>MENU</b></li>
+	<li><a href ="#">Menu 1</a></li>
+	<li><a href ="#">Menu 2</a></li>
+	<li><a href ="#">Menu 3</a></li>
+
+	</ul>
+</nav>
+
+	</nav>
+	<article>
+		<br></br>
+		<table border="1" width="50%" align="center">
+
+			<td>
+				<th><a href="Tambah.php"><button type="submit">TAMBAH</button></a></th>
+			</td>
+			<td>
+				<th><a href="edit.php"><button type="submit">EDIT</button></a></th>
+			</td>
+			<tr>
+				<td align=center>No</td>
+				<td align=center>Nama</td>
+				<td align=center>Nim</td>
+				<td align=center>Alamat</td>
+				<td align=center>Aksi</td>
+			</tr>
+		</table>
+	</article>
+
+<footer>
+<h2>Hesar Nur Ramaduloh (14111218)</h2>
+<h3> "14 TIF Reguler malan" </h3>
+</footer>
+</div>
+</body>
+</html>
